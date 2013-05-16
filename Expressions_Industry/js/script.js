525b41aaa5c923d46a26678f8c61f7a2bbd513b6
Ednalda <ednaldafakira@gmail.com>
2013-05-16 17:33:20 -0400

    add the variables that define the resolution

---
+++ Expressions_Industry/js/script.js
@@ -0,0 +1,10 @@
+//Ednalda Fakira  05/16/2013 Expressions_industry
+
+var width = 3;
+var height = 3;
+var dpi = 100;
+
+var resolution = (width * dpi) * (height * dpi);
+var resultPixel = "The resolution is " + resultion + "pixels"
+
+
