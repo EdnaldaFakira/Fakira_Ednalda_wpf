183c90c894e18001b338036d71f95c620f286b8c
Ednalda <ednaldafakira@gmail.com>
2013-05-16 17:43:29 -0400

    add the variable widht, height, dpi, and defines the values as a prompt command to imput values

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,9 +1,15 @@
 //Ednalda Fakira  05/16/2013 Expressions_industry
 
+
+var prompt(" Please enter the width of the photo ");
+var prompt(" Please enter the height of the photo ");
+var prompt(" Please enter the dpi of the photo ");
 var width = 3;
 var height = 3;
 var dpi = 100;
 
+
 var resolution = (width * dpi) * (height * dpi);
-var resultPixel = "The resolution is " + resultion + "pixels"
+var resultPixel = "The resolution is " + resultion + "pixels";
 console.log(resultPixel);
+
