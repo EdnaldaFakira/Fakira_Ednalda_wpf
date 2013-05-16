1d697ca3d7763ec4222b96366bf6b0d3dea69e4d
Ednalda <ednaldafakira@gmail.com>
2013-05-16 17:48:52 -0400

    delete the variables width, height, dpi and their values as 3,3,100 respectives

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,15 +1,13 @@
 //Ednalda Fakira  05/16/2013 Expressions_industry
 
 
-var prompt(" Please enter the width of the photo ");
-var prompt(" Please enter the height of the photo ");
-var prompt(" Please enter the dpi of the photo ");
-var width = 3;
-var height = 3;
-var dpi = 100;
+var  width = prompt(" Please enter the width of the photo ");
+var height = prompt(" Please enter the height of the photo ");
+var  dpi   = prompt(" Please enter the dpi of the photo ");
+
 
 
 var resolution = (width * dpi) * (height * dpi);
 var resultPixel = "The resolution is " + resultion + "pixels";
-console.log(resultPixel);
+console.log(resultPixel); 
 
