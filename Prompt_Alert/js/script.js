08ebf8fc5ab55878f04eef88b9cf07c635110afc
Ednalda <ednaldafakira@gmail.com>
2013-05-16 00:10:11 -0400

    The result of the area is visible.

--- Prompt_Alert/js/script.js
+++ Prompt_Alert/js/script.js
@@ -5,4 +5,5 @@ var height = prompt("Please enter the height");
 var area = widht * height; // calculates with that info
 console.log(area); //print it out to console.
 
-
+var result = "The area of your rectangle is " + area + "sq feet"
+alert(result);
