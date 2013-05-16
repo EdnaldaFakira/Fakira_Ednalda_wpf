32974f67c474b92149645513421a593f28048e0e
Ednalda <ednaldafakira@gmail.com>
2013-05-16 17:57:08 -0400

    add array to show message about informations need to calculate the photo resolution

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,5 +1,9 @@
 //Ednalda Fakira  05/16/2013 Expressions_industry
 
+var infoneed = new Array();
+infoneed [0] = "Your photo dimensions";
+infoneed [1] = "Your photo pixelsm or dpi";
+
 
 var  width = prompt(" Please enter the width of the photo ");
 var height = prompt(" Please enter the height of the photo ");
@@ -8,9 +12,6 @@ var  dpi   = prompt(" Please enter the dpi of the photo ");
 console.log(width);
 console.log(height);
 console.log(dpi);
-
-
-
 
 var resolution = (width * dpi) * (height * dpi);
 var resultPixel = "The resolution is " + resultion + "pixels";
