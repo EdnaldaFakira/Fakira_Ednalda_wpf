5998c17ce48ec426c375ff5d423e422a8e6815b6
Ednalda <ednaldafakira@gmail.com>
2013-05-16 18:28:01 -0400

    Add the comments each line

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,21 +1,21 @@
 //Ednalda Fakira  05/16/2013 Expressions_industry
 
-var infoNeed = new Array();
-infoNeed [0] = "Your photo dimensions";
-infoNeed [1] = "Your photo dpi";
-totalInfoNeed = "Your photo dimensions" + "Your photo dpi";
-console.log(totalInfoNeed);
+var infoNeed = new Array(); //variable defines as array
+infoNeed [0] = "Your photo dimensions"; //first position on the array
+infoNeed [1] = "Your photo dpi"; //second position on the array
+totalInfoNeed = "Your photo dimensions " [0] + "Your photo dpi" [1]; //the total of the addition of all position of the array
+console.log(totalInfoNeed); //print out the result of the total 
 
 
-var  width = prompt(" Please enter the width of the photo ");
-var height = prompt(" Please enter the height of the photo ");
-var  dpi   = prompt(" Please enter the dpi of the photo ");
+var  width = prompt(" Please enter the width of the photo "); //imput the width value
+var height = prompt(" Please enter the height of the photo "); //imput the height value
+var  dpi   = prompt(" Please enter the dpi of the photo "); //imput the dpi value
 
-console.log(width);
-console.log(height);
-console.log(dpi);
+console.log(width); //print out the width value
+console.log(height); //print out the height value
+console.log(dpi); //print out the dpi value
 
-var resolution = (width * dpi) * (height * dpi);
-var resultPixel = "The resolution is " + resultion + "pixels";
-console.log(resultPixel); 
+var resolution = (width * dpi) * (height * dpi); //declares and defines the resolution variable as a result of calculating values
+var resultPixel = "The resolution of your photo is " + resultion + "pixels"; //declares the variable that show as a expression the resolution
+console.log(resultPixel); //print out the resolution  
 
