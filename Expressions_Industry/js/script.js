ee9f95b08f15a445eed9d05588313a24cd86ef5b
Ednalda <ednaldafakira@gmail.com>
2013-05-16 20:01:53 -0400

    add the variable that calculates the area of photo

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -10,6 +10,9 @@ console.log(height); //print out the height value
 console.log(dpi); //print out the dpi value
 
 var resolution = (width * dpi) * (height * dpi); //declares and defines the resolution variable as a result of calculating values
-var resultPixel = "The resolution of your photo is " + resolution + "pixels"; //declares the variable that show as a expression the resolution
+var resultPixel = "The resolution of your photo is " + resolution + " pixels per total sq ft"; //declares the variable that show as a expression the resolution
 console.log(resultPixel); //print out the resolution  
 
+var sqFt = [width, height];
+var totalSqFt = width [0] * height[1];
+console.log(totalSqFt);
