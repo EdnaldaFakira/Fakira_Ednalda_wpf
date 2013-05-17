0456ed04e392f310eac80db5fda4d0ed1930c6d4
Ednalda <ednaldafakira@gmail.com>
2013-05-16 21:31:21 -0400

    delete one line of the array because of repetance of values

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,25 +1,25 @@
 //Ednalda Fakirs 05/16/2013 Expression_Wacky
 
-var height = prompt(" Please enter the Height of your cabinet ");
-var width + prompt(" Please enter the width of your cabinet ");
-var totalArea = height * width
-var area = "The total area of your cabinet is " + totalArea + "sq ft";
+var height = prompt(" Please enter the Height of your cabinet "); //declares variable and defines the  height imput value
+var width + prompt(" Please enter the width of your cabinet "); //declares variable and defines the width imput value
+var totalArea = height * width; //declared variable and defines value as totalArea
+var area = "The total area of your cabinet is " + totalArea + "sq ft"; //declares variable and defines as the area in a sentence
 
-console.log(height);
-console.log(widht);
-console.log(area);
+console.log(height); //print out the height value
+console.log(widht); //print out the  width value
+console.log(area); //print out the  area value
 
-var price = totalArea * 10 + (totalArea * 3 / 100);
-var totalPrice = " The price of your cabinet is $" + price;
+var price = totalArea * 10 + (totalArea * 3 / 100); //declares variable and defines the price as a result of calculating the totalArea times $10 + 6% of sales commission
+var totalPrice = " The price of your cabinet is $" + price; //declares variable and it shows the totalPrice in a sentence
 
-var tax = totalPrice + (totalPrice * 6 / 100);
-var fianalPrice = totalPrice + tax;
-var finalPriceDo = " The total price of each cabinet with sales tax included is $" + finalPrice;
-console.log(finalPriceDo);
+var tax = totalPrice + (totalPrice * 6 / 100); //declares variable and defines tax value of 6% from the totalPrice
+var finalPrice = totalPrice + tax; //declares variable and defines finalPrice as totalPrice + tax
+var finalPriceDo = " The total price of each cabinet with sales tax included is $" + finalPrice; //declares variable and it shows the result in the setence
+console.log(finalPriceDo); //print out the finalPriceDo value
 
-var cabinet = [5, 12, 15, 20, 4, 9, 0]
-var cabinetQy = 5[0] + 12[1];
-var totalCabinet = cabinetQy;
+var cabinet = [5, 12, 15, 20, 4, 9, 0]; //declares variable cabinet and defines the  sequency of week days as 0 = sunday
+var cabinetQy = 5[0] + 12[1]; //declares variable and defines how many cabinets sold this week
+ //declares variable totalCabinet and defines 
 console.log(totalCabinet);
 
 var wood = [pine, oak, cherry];
