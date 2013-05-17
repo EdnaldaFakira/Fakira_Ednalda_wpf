cbc4739f52baa426c14b397fc3464a8249c73c0b
Ednalda <ednaldafakira@gmail.com>
2013-05-16 20:34:50 -0400

    add variables  height, width, totalArea and defines the values as imput

---
+++ Expressions_Wacky/js/script.js
@@ -0,0 +1,9 @@
+//Ednalda Fakirs 05/16/2013 Expression_Wacky
+
+var height = prompt(" Please enter the Height of your cabinet ");
+var width + prompt(" Please enter the width of your cabinet ");
+var totalArea = height * widht;
+
+console.log(height);
+console.log(widht);
+console.log(totalArea);
