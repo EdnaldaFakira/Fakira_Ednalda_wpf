91b16f95d0125408c6943c2b98b0b435075e16e0
Ednalda <ednaldafakira@gmail.com>
2013-05-16 20:55:22 -0400

    add tax variable to caclute 6% of tax

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -11,6 +11,8 @@ console.log(area);
 
 var price = totalArea * 10 + (totalArea * 3 / 100);
 var totalPrice = " The price of your cabinet is $" + price;
+var tax = totalPrice + (totalPrice * 6 / 100);
+var finalPrice = " The total price of your cabinet with sales tax included is $" + finalPrice;
 
 var wood = [pine, oak, cherry];
 var allWood = pine[0];
