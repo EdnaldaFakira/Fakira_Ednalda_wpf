b7d06be79f051562f29d1825a2ec91a56574b923
Ednalda <ednaldafakira@gmail.com>
2013-05-23 13:03:33 -0400

    add comments

--- Conditionals_Worksheet/Movie Ticket Price/js/script.js
+++ Conditionals_Worksheet/Movie Ticket Price/js/script.js
@@ -1,5 +1,7 @@
-var movieTime = 7;
-var ageCustomer = 50;
+//Ednalda Fakira 05/23/2013 Conditionals Worksheet - Movie Ticket Price
 
- (ageCustomer >= 55 || ageCustomer > 10 || movieTime 3 >= 5) ? console.log ("The Ticket price is $" + 5) : console.log("The Ticket Price is $" + 12);
- 
+var movieTime = 7; //watching movie this time
+var ageCustomer = 50; //the age of the customer
+
+ (ageCustomer >= 55 || ageCustomer > 10 || movieTime 3 >= 5) ? console.log ("The Ticket price is $" + 5) : console.log("The Ticket Price is $" + 12); //condition of to print ticket price: if the customer is or over 55 yars old, or less then 10 years eld, or wacht movie between 3 to 5 pm, get $7.00 discount, if none of this conditions the ticket price is $12.00
+
