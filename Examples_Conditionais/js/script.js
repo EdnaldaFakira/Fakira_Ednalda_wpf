48b9cce12db7d31180db5c4f4fe5bafdbe8d724c
Ednalda <ednaldafakira@gmail.com>
2013-05-23 09:19:09 -0400

    add the if expressing the condition of the kid heightsis less than the minimum height  to print the answer

--- Examples_Conditionais/js/script.js
+++ Examples_Conditionais/js/script.js
@@ -5,3 +5,7 @@ if (kidHeight  + sneakerLift > minHeight){console.log("You can ride the coaster!
 
 }  //code performed if the condintion is true. if the kid is over 48 inches in height
 
+if (kidHeight < minHeight){
+	console.log( "Sorry kid, you've go some grow to do first");
+	
+}
