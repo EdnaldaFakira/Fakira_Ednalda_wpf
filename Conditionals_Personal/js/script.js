0d35fb50e56d32f593ebd4024978c43dbcd0f72a
Ednalda <ednaldafakira@gmail.com>
2013-05-23 14:05:47 -0400

    add variables, defines variables and add comments

---
+++ Conditionals_Personal/js/script.js
@@ -0,0 +1,10 @@
+//Ednalda Fakira 05/23/2013 Conditional Personal
+
+var age = prompt("Please enter your age"); //Custormer enter his age
+var flightTime = prompt("Please enter the time you want to travel"); //Customer enter the time wish to travel
+var hotel = 70; //price the hotel per night
+var carRent = 30; //price the car rent per day
+var priceTicket; //price of the flight ticket
+var totalPrice; //total price of the vacation packet calculating the priceticket, how many days in a hotel and car rented
+var totalPrice = priceTicket; // 
+
--- Conditionals_Worksheet/Check Login/js/script.js
+++ Conditionals_Worksheet/Check Login/js/script.js
@@ -6,11 +6,11 @@ var password = prompt("Please enter your password"); //user imput the password
 var correctPassword; //password storaged
 var correctUserName; //username storaged
 
-if(userName == correctUserName && password != correctPassword){
+if(userName === correctUserName && password != correctPassword){
  console.log("Password does match our records."); //username matches but the password doesn't match
 } 
 
-else if (password == correctPassword && userName != correctName){
+else if (password === correctPassword && userName != correctName){
  console.log("User not found. Try again."); //password matches but username doesn' match
 }
 
