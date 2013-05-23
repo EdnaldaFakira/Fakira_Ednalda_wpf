38ec83088a47ae42fb2e046ae3b3fb3151d43531
Ednalda <ednaldafakira@gmail.com>
2013-05-23 19:29:59 -0400

    add comments to the age variable as defined value prompt command to enter the age

--- Conditional_Wacky/js/script.js
+++ Conditional_Wacky/js/script.js
@@ -1,19 +1,19 @@
 //Ednalda Fakira 05/23/2013 Conditional Wacky
 
 
-var age = prompt("How old are you?");
-var state = prompt("Please enter the state you live");
+var age = prompt("How old are you?"); //declares the variable age and defines value as prompt
+var state = prompt("Please enter the state you live"); 
 var legal = prompt("Are you citizens or legal resident");
 var cost = 50;
 var costFlorida = 50 * 6 / 100 + cost;
 var costOtherState = 50 * 8 / 100 + cost;
 
 if (age >= 18 && legal === yes && state === "Florida"){
-	console.log("You can get your drive license. The price is $" + costFlorida);
+	console.log("You can get your driver license. The price is $" + costFlorida);
 }
 
 if (age >= 18 && legal === yes && state !=== "Florida"){
-	console.log("You can get your drive license. The price is $" + costOtherState);
+	console.log("You can get your driver license. The price is $" + costOtherState);
 }
 
 if else (age >= 15 && legal === yes && state == "Florida"){
@@ -29,6 +29,6 @@ if else (age === "" || state === "" || legal === ""){
  }
 
 else {
-	console.log("You can't have drive license yeat");
+	console.log("You can't have driver license yeat");
 }
 
