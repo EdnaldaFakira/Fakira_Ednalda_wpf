e19ce17fc9be38e531f74f267503e1690f643b26
Ednalda <ednaldafakira@gmail.com>
2013-05-23 09:54:13 -0400

    add the condition paycheck > 300 using && operator

--- Conditional_Expressions/js/script.js
+++ Conditional_Expressions/js/script.js
@@ -2,10 +2,11 @@ var budget = 300;
 var iPhonePrice = 199.99;
 var payCheck = 200;
 
-if(iPhonep\Price < budget){console.log("We can buy the phone");
+if(iPhonepPrice < budget && payCheck > 300){console.log("We can buy the phone"); //the iphone is less than the budget we can buy iphone
 
 }
 
-else {console.log("No phone for you!");
+}
+else {console.log("No phone for you!");// if the budget is greater than the iphone price no phone for us
 
 } 
