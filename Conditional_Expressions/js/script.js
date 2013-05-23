17bd9e2d0936457b7746ac04f65bd6fb8231febf
Ednalda <ednaldafakira@gmail.com>
2013-05-23 09:58:58 -0400

    add the condition of (or) to execute the message " We can buy the phone" represented by the operator ||

--- Conditional_Expressions/js/script.js
+++ Conditional_Expressions/js/script.js
@@ -1,12 +1,12 @@
 var budget = 300;
 var iPhonePrice = 199.99;
 var payCheck = 200;
+var wonLottery = true;
 
-if(iPhonepPrice < budget && payCheck > 300){console.log("We can buy the phone"); //the iphone is less than the budget we can buy iphone
+if(iPhonepPrice < budget && payCheck > 300 || wonLottery){console.log("We can buy the phone"); //the iphone is less than the budget an the paycheck is greater than 300, we can buy iphone
 
 }
 
-}
 else {console.log("No phone for you!");// if the budget is greater than the iphone price no phone for us
 
 } 
