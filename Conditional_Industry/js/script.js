dbee6ca57898f68dfe8867975db3a28d210fe3ce
Ednalda <ednaldafakira@gmail.com>
2013-05-23 17:16:52 -0400

    set variable widowWidth, windowhight and defines value as prompt commnad. also declare variable windowSize that calcules the windowhight times windowWidth

---
+++ Conditional_Industry/js/script.js
@@ -0,0 +1,5 @@
+//Ednalda Fakira 05/23/2013 Conditional Industry
+
+var windowWidth = prompt("Please enter the width");
+var windowHeight = prompt("Please enter the height");
+var windowSize = windowWidth * windowHeight;
