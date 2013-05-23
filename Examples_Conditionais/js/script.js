bac3c43d8cdf532ec87e88f6ea683f44a7cac7d1
Ednalda <ednaldafakira@gmail.com>
2013-05-23 09:13:39 -0400

    substitute the value 2 to the variable sneakerLift

--- Examples_Conditionais/js/script.js
+++ Examples_Conditionais/js/script.js
@@ -1,6 +1,7 @@
 var kidHeight = 40;
 var minHeight = 48;
-if (kidHeight  + 2 > minHeight){console.log("You can ride the coaster!");           
+var sneakerLift = 2;
+if (kidHeight  + sneakerLift > minHeight){console.log("You can ride the coaster!");           
 
 }  //code performed if the condintion is true. if the kid is over 48 inches in height
 
