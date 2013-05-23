a8636bc30267f0a746659cac6340f08536af8ec3
Ednalda <ednaldafakira@gmail.com>
2013-05-23 09:31:16 -0400

    add the condition else if to execute the condion of kid height with a parent present minimum height

--- Examples_Conditionais/js/script.js
+++ Examples_Conditionais/js/script.js
@@ -1,9 +1,15 @@
-var kidHeight = 40;
-var minHeight = 48;
-var sneakerLift = 2;
+var kidHeight = 44; //the kid height
+var minHeight = 48; //the minimum height to ride
+var sneakerLift = 2; //the sneak height added
+var wParentHeight = 43: //the kid height with the parente
+
 if (kidHeight  + sneakerLift > minHeight){console.log("You can ride the coaster!");           
 
 }  //code performed if the condintion is true. if the kid is over 48 inches in height
+
+else if(kidHeight > wParentHeight){ "You can ride, but only with a parent present."
+
+} //you can ride if you have your parent present
 
 else (kidHeight < minHeight){
 	console.log( "Sorry kid, you've go some grow to do first");
