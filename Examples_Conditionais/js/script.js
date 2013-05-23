15298e8b4ef06616bedee5430901217b89a1c9bf
Ednalda <ednaldafakira@gmail.com>
2013-05-23 09:04:35 -0400

    Add the console to print out "What comes after", but nothing happened because the condintion is not add.

--- Examples_Conditionais/js/script.js
+++ Examples_Conditionais/js/script.js
@@ -2,3 +2,4 @@ var oldEnogh = false;
 if (oldEnogh){console.log("You can ride the coaster!");           
 
 }  //code performed if the condintion is true. If the child is old enough, print to the console "you can ride!"
+console.log("What comes after");
