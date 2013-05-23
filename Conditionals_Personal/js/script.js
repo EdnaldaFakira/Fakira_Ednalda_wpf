6800a9372b969121f4f5268694f850ac01578f32
Ednalda <ednaldafakira@gmail.com>
2013-05-23 15:04:40 -0400

    add destination variable and define the value as Florida

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,5 +1,6 @@
 //Ednalda Fakira 05/23/2013 Conditional Personal
 
+var destination = "Florida"
 var age = prompt("Please enter your age"); //Custormer enter his age
 var flightTime = prompt("Please enter the time you want to travel"); //Customer enter the time wish to travel
 var hotelDays = prompt("Please enter how many days you want to stay");
@@ -8,7 +9,7 @@ var hotel = 70; //price the hotel per night
 var carRent = 30; //price the car rent per day
 var priceTicket = 200; //price of the flight ticket
 var totalPrice = priceTicket + hotelDays * hotel + carTakes * carRent; //total price of the vacation packet calculating the priceticket, how many days in a hotel and car rented
- 
+
 if (flightTime 22 >= 6) {
 	console.log("You have a 10% of descont of your  flight ticket");
 	//customer gets 10% of descont if choose to travel betwee 10 PM and 6 AM
