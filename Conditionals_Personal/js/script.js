9abcae66455a8904fc7ea8c9f1b2a8d8bb99fa17
Ednalda <ednaldafakira@gmail.com>
2013-05-23 16:09:06 -0400

    makes all the conditions as comments, and add ternary operatores to reduce  the code to one line

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -10,16 +10,27 @@ var carRent = 30; //price the car rent per day
 var priceTicket = 200; //price of the flight ticket
 var totalPrice = priceTicket + hotelDays * hotel + carTakes * carRent; //total price of the vacation packet calculating the priceticket, how many days in a hotel and car rented
 
-if (flightTime 6 =< 22 && destinationEnter === "Florida" || age > 12) {
-	console.log("You have a 10% of discount and your vacation packet is $" + totalPrice * 10 / 100);
+//if (flightTime 6 =< 22 && destinationEnter === "Florida" || age > 12) {
+	//console.log("You have a 10% of discount and your vacation packet is $" + totalPrice * 10 / 100);
 	//customer gets 10% of discount if choose to travel between 10:00 PM and 6:00 AM
-}
-else if (flightTime 6 > 22 && destinationEnter === "Florida") {
-	console.log("You have 5% of discount and your vacation packet is $" + totalPrice * 5 / 100);
+//}
+//else if (flightTime 6 > 22 && destinationEnter === "Florida") {
+	//console.log("You have 5% of discount and your vacation packet is $" + totalPrice * 5 / 100);
 	//traveling betwen 6:00 AM and 9:59 PM to Florida get 5% of discount 
-}
+//}
 
-else {
-	console.log("Your vacation packet price is $" + totalPrice);
+//else {
+	//console.log("Your vacation packet price is $" + totalPrice);
 	//print the vacation packet price without discount
-}
+//}
+
+(flightTime 6 =< 22 && destinationEnter === "Florida" || age > 12) ? console.log("You have 5% of discount and your vacation packet is $" + totalPrice * 5 / 100) : (flightTime 6 > 22 && destinationEnter === "Florida")
+
+
+
+
+
+
+
+
+
