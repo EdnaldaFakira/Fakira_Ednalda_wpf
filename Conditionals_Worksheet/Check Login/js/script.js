1a540ed8588da9085a243db830009e3b2a87f903
Ednalda <ednaldafakira@gmail.com>
2013-05-23 12:26:05 -0400

    add conditionals to print username and password match or not mach the correct ones

---
+++ Conditionals_Worksheet/Check Login/js/script.js
@@ -0,0 +1,18 @@
+//Ednalda Fakira 05/23/2013 Conditionals Worksheet - Check the login
+
+
+var userName = prompt("Please enter your user name");
+var password = prompt("Please enter your password");
+var correctPassword 
+var correctUserName
+
+if(userName == correctUserName && password != correctPassword){
+ console.log("Password does match our records.");
+} 
+
+else if (password == correctPassword && userName != correctName){
+ console.log("User not found. Try again.");
+}
+
+else {console.log("Welcome," + correctUserName);}
+
