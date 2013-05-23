aaf13af6fd0a51320e096e6cf52d3f969a678a1b
Ednalda <ednaldafakira@gmail.com>
2013-05-23 10:25:39 -0400

    add the ternary operator to combine the condition to execute the message according to the age gets a different book

--- TernaryOperators/js/script.js
+++ TernaryOperators/js/script.js
@@ -1,7 +1,16 @@
-var gpa = 48; //if the gpa is over 2.0, the student can graduate
+//var gpa = 48; //if the gpa is over 2.0, the student can graduate
+
 //if(gpa > 2.0){
 	console.log("You can graduate!");
 }
 //else {"GPA is too low!"};
 
-(gpa > 2.0) ? console.log("you can graduate!") : console.log("GPA is too low");
+//(gpa > 2.0) ? console.log("you can graduate!") : console.log("GPA is too low");
+
+
+
+
+var book;
+var age = 9;
+book = (age < 11) ? "Green Eggs and Ham" : "The Time Machine"; //if the child is less than 11 years old, print the book "Eggs and Ham", if is the child is over 11 years old, print the book "Time Machine" 
+console.log(book);
