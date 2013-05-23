7199498563bbbdb41452f5901fdaafbd412b73a5
Ednalda <ednaldafakira@gmail.com>
2013-05-23 09:12:15 -0400

    add 2 inches to the kid height as a sneaker height

--- Examples_Conditionais/js/script.js
+++ Examples_Conditionais/js/script.js
@@ -1,6 +1,6 @@
 var kidHeight = 40;
 var minHeight = 48;
-if (kidHeight > minHeight){console.log("You can ride the coaster!");           
+if (kidHeight  + 2 > minHeight){console.log("You can ride the coaster!");           
 
 }  //code performed if the condintion is true. if the kid is over 48 inches in height
 
