1e8bc6c732a80351df1bf69f6021d72122ae9972
Ednalda <ednaldafakira@gmail.com>
2013-05-23 14:59:18 -0400

    add hotelDays variable and defines as prompt command

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -2,9 +2,15 @@
 
 var age = prompt("Please enter your age"); //Custormer enter his age
 var flightTime = prompt("Please enter the time you want to travel"); //Customer enter the time wish to travel
+var hotelDays = prompt("Please enter how many days you want to stay");
+var carTakes = hotelDays; //the number of days the customer rent the car is the same of the number of days in the hotel
 var hotel = 70; //price the hotel per night
 var carRent = 30; //price the car rent per day
-var priceTicket; //price of the flight ticket
-var totalPrice; //total price of the vacation packet calculating the priceticket, how many days in a hotel and car rented
-var totalPrice = priceTicket; // 
-
+var priceTicket = 200; //price of the flight ticket
+var totalPrice = priceTicket + hotelDays * hotel + carTakes * carRent; //total price of the vacation packet calculating the priceticket, how many days in a hotel and car rented
+ 
+if (flightTime 22 >= 6) {
+	console.log("You have a 10% of descont of your  flight ticket");
+	//customer gets 10% of descont if choose to travel betwee 10 PM and 6 AM
+}
+else if ()
