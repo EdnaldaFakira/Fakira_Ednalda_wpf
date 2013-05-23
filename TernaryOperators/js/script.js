03957dc30fad41097e3f1ae93a93bcdde047df94
Ednalda <ednaldafakira@gmail.com>
2013-05-23 10:16:45 -0400

    add tenary operators "?" for if, and ": "for else to conbine condition in one line

---
+++ TernaryOperators/js/script.js
@@ -0,0 +1,7 @@
+var gpa = 48; //if the gpa is over 2.0, the student can graduate
+//if(gpa > 2.0){
+	console.log("You can graduate!");
+}
+//else {"GPA is too low!"};
+
+(gpa > 2.0) ? console.log("you can graduate!") : console.log("GPA is too low");
