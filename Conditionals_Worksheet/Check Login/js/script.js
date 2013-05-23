d7e0be08b1dcfa299cc1717d993149439b63eb9d
Ednalda <ednaldafakira@gmail.com>
2013-05-23 12:58:22 -0400

    add the ternary operator to reduce to a single line

--- Conditionals_Worksheet/Check Login/js/script.js
+++ Conditionals_Worksheet/Check Login/js/script.js
@@ -1,18 +1,19 @@
 //Ednalda Fakira 05/23/2013 Conditionals Worksheet - Check the login
 
 
-var userName = prompt("Please enter your user name");
-var password = prompt("Please enter your password");
-var correctPassword 
-var correctUserName
+var userName = prompt("Please enter your user name"); //user imput the username 
+var password = prompt("Please enter your password"); //user imput the password
+var correctPassword; //password storaged
+var correctUserName; //username storaged
 
 if(userName == correctUserName && password != correctPassword){
- console.log("Password does match our records.");
+ console.log("Password does match our records."); //username matches but the password doesn't match
 } 
 
 else if (password == correctPassword && userName != correctName){
- console.log("User not found. Try again.");
+ console.log("User not found. Try again."); //password matches but username doesn' match
 }
 
-else {console.log("Welcome," + correctUserName);}
+else {console.log("Welcome," + correctUserName); //password and username match
+}
 
---
+++ Conditionals_Worksheet/Movie Ticket Price/js/script.js
@@ -0,0 +1,5 @@
+var movieTime = 7;
+var ageCustomer = 50;
+
+ (ageCustomer >= 55 || ageCustomer > 10 || movieTime 3 >= 5) ? console.log ("The Ticket price is $" + 5) : console.log("The Ticket Price is $" + 12);
+ 
