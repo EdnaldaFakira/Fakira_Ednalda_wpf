372c102f20440e0ec94e4a00555089e94430f253
Ednalda <ednaldafakira@gmail.com>
2013-05-23 13:13:54 -0400

    add comments of each line

---
+++ Examples_Conditionals/js/script.js
@@ -0,0 +1,19 @@
+var kidHeight = 44; //the kid height
+var minHeight = 48; //the minimum height to ride
+var sneakerLift = 2; //the sneak height added
+var wParentHeight = 43: //the kid height with the parente
+
+if (kidHeight  + sneakerLift > minHeight){console.log("You can ride the coaster!");           
+
+}  //code performed if the condintion is true. if the kid is over 48 inches in height
+
+else if(kidHeight > wParentHeight){ console.log("You can ride, but only with a parent present.");
+
+} //you can ride if you have your parent present
+
+else (kidHeight < minHeight){
+	console.log( "Sorry kid, you've go some grow to do first");
+
+} //if the kid height is less than the minimum height print the answer 
+ 
+ 
