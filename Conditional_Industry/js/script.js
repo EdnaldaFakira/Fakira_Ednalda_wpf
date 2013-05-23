8e2f396ed5b434132f0d05b4c0f43a954772465d
Ednalda <ednaldafakira@gmail.com>
2013-05-23 18:17:12 -0400

    extended the "else" condition as windowColorEnter value need

--- Conditional_Industry/js/script.js
+++ Conditional_Industry/js/script.js
@@ -2,22 +2,22 @@
 
 var windowWidth = prompt("Please enter the width"); //enter the width value
 var windowHeight = prompt("Please enter the height"); //enter the height value
-var windowcolorEnter = prompt("Please enter the window color: white, black or blue"); //enter the color as white, black or blue
+var windowColorEnter = prompt("Please enter the window color: white, black or blue"); //enter the color as white, black or blue
 var windowColor = "white"; //variable windowColor defines the value as "white"
 var windowSize = windowWidth * windowHeight; //variable windowSize calcules the windowWidth times windowHeight
 
-if (windowWidth === windowHeight && windowcolorEnter === "black" || windowColorEnter === "blue"){
+if (windowWidth === windowHeight && windowColorEnter === "black" || windowColorEnter === "blue"){
 	console.log("Your window is square with" + windowSize + "sq and is good for commercial websites");
 	//print the value as a message and the area of the window
 }
 
-if else (windowHeight > windowWidth || windowWidth > windowHeight && windowcolorEnter === "white"){
+if else (windowHeight > windowWidth || windowWidth > windowHeight && windowColorEnter === "white"){
 	console.log("Your window is rectangle" + windowSize + "sq and is good for educational websites");
 	//print the value as a message showing that the window is a rectagle and the area of the window 
 }
 
 
-else (windowHeight == "" || windowWidth == "" ){
+else (windowHeight == "" || windowWidth == "" || windowColorEnter === "" ){
 	console.log("You need to enter width and height");
 	//print a message to remenber to enter the width and height to calcletes the area 
 }
