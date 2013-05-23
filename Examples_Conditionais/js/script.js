ae8c833c7cfc26f6180ca39c69b4ee5422a084d7
Ednalda <ednaldafakira@gmail.com>
2013-05-23 09:21:43 -0400

    substitutes the second if to else

--- Examples_Conditionais/js/script.js
+++ Examples_Conditionais/js/script.js
@@ -5,7 +5,9 @@ if (kidHeight  + sneakerLift > minHeight){console.log("You can ride the coaster!
 
 }  //code performed if the condintion is true. if the kid is over 48 inches in height
 
-if (kidHeight < minHeight){
+else (kidHeight < minHeight){
 	console.log( "Sorry kid, you've go some grow to do first");
-	
-}
+
+} //if the kid height is less than the minimum height print the answer 
+ 
+ 
