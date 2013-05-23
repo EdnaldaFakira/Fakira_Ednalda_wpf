1280b3876bda32174a6d0a1fb02efceed1e58a41
Ednalda <ednaldafakira@gmail.com>
2013-05-23 18:34:15 -0400

    declares the variables age, state, legal, and defines the values as prompt command

---
+++ Conditional_Wacky/js/script.js
@@ -0,0 +1,7 @@
+//Ednalda Fakira 05/23/2013 Conditional Wacky
+
+
+var age = prompt("How old are you?");
+var state = prompt("Please enter the state you live");
+var legal = prompt("Are you citizens or legal resident");
+
