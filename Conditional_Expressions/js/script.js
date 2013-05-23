fc80a5aa324095e75fc250fe5298cdee827dc25a
Ednalda <ednaldafakira@gmail.com>
2013-05-23 09:50:36 -0400

    add the else condition to print the answer according to the budget less than iPhonePrice

---
+++ Conditional_Expressions/js/script.js
@@ -0,0 +1,11 @@
+var budget = 300;
+var iPhonePrice = 199.99;
+var payCheck = 200;
+
+if(iPhonep\Price < budget){console.log("We can buy the phone");
+
+}
+
+else {console.log("No phone for you!");
+
+} 
