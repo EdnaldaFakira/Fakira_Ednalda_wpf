d677f546147de4a4958e85b55556038bef663ebf
Ednalda <ednaldafakira@gmail.com>
2013-05-23 20:20:32 -0400

    turns the conditional staements to ternaries operators to reduce statement to on line code

--- Conditional_Industry/js/script.js
+++ Conditional_Industry/js/script.js
@@ -5,18 +5,7 @@ var windowHeight = prompt("Please enter the height"); //enter the height value
 var windowColorEnter = prompt("Please enter the window color: white, black or blue"); //enter the color as white, black or blue
 var windowSize = windowWidth * windowHeight; //variable windowSize calcules the windowWidth times windowHeight
 
-if (windowWidth === windowHeight && windowColorEnter === "black" || windowColorEnter === "blue"){
-	console.log("Your window is square with" + windowSize + "sq and is good for commercial websites");
-	//print the value as a message and the area of the window
-}
+(windowWidth === windowHeight && windowColorEnter === "black" || windowColorEnter === "blue") ? console.log("Your window is square with" + windowSize + "sq and is good for commercial websites") : console.log("Your window is rectangle" + windowSize + "sq and is good for educational websites");
+	//print the value as a message showing that the window is a square, the area of the window, and is good for commercial websites 
+	//print the value as a message showing that the window is a rectagle, the area of the window, and is good for educational websites
 
-if else (windowHeight > windowWidth || windowWidth > windowHeight && windowColorEnter === "white"){
-	console.log("Your window is rectangle" + windowSize + "sq and is good for educational websites");
-	//print the value as a message showing that the window is a rectagle and the area of the window 
-}
-
-
-else (windowHeight == "" || windowWidth == "" || windowColorEnter === "" ){
-	console.log("You need to enter width and height");
-	//print a message to remenber to enter the width and height to calculetes the area, and the color
-}
