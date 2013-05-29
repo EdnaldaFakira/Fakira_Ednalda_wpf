6b344deea0b09628decb6ed14626dee1fcfbb2b8
Ednalda <ednaldafakira@gmail.com>
2013-05-29 15:33:56 -0400

    changes the comment explaining the var circumference as the radius * Pi 3.14

--- Functions_worksheet/script/script.js
+++ Functions_worksheet/script/script.js
@@ -1,11 +1,13 @@
 //Ednalda Fakira Functions Worsheet 05/29/13
 
+
+
 //Calculates the circumference of the circle
 
 var totalCircumference = calcCircumference(5);//Defines the variable and defines value as the variable calcCircumference and values as 5 and 2
 
 function calcCircumference(radius){ //Defines the function that hold the parameter radius
-var circumference = radius * 3.14;//Defines the variable circumference and defines value as variable diameter / 2.
+var circumference = radius * 3.14;//Defines the variable circumference and defines value as radius * 3.14 (circumference / diameter = Pi 3.14).
 return circumference //function spitting the info out
 }
 
@@ -13,13 +15,17 @@ console.log("The circumference of the circle is" + totalCircumference); //print 
 
 
 
+
+
 //Calculates the amount of bee stings to kill an animal
 
 var totalStings =  calcBeeStings(130);//defines the variable totalStings and defines value as variable calcBeeStings as 130 pounds
+
 function calcBeeStings (victimWeight){ //defines function calcBeeStings to store the value as parameter victimWeght
 	var beeStings = victimWeight * 8.7; //defines variable beeStings and defines value as victimWeight * 8.7 (number of stings per pound to kill an animal)
 	return beeStings //it returns the value in the function
 }
+
 console.log("It takes" + totalStings + "to kill a" + victimWeight + "pounds animal");//print out the totalStings required to kill the victimWeight in pounds
 
 
