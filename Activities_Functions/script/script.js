bb1eef6a4a00b73fee37df3c6a5cff737244939a
Ednalda <ednaldafakira@gmail.com>
2013-05-29 11:20:25 -0400

    add variable width as scope and print console as a result

--- Activities_Functions/script/script.js
+++ Activities_Functions/script/script.js
@@ -1,14 +1,18 @@
 unction outMsg(){
-	console.log("Hello World");
+	console.log("Hello World"); //print the message defines by function
 }
+
+var width = 5; //variable scope
+
  function calcArea(){
  	var width = 20;
  	var height = 30;
- 	var area = widht * height;
- 	console.log(area);
+ 	var area = widht * height; //calculates the variable area by variable width * variable height
+ 	console.log(area); //print the message defines by function
  }
 
+ calcArea(); //invokes the function
  calcArea();
  calcArea();
- calcArea();
- 
+
+console.log(width); //print variable widht
