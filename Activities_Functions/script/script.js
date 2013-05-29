3edaf42cfbd7e73464ea6304bad520adf8b97750
Ednalda <ednaldafakira@gmail.com>
2013-05-29 11:47:26 -0400

    add parameters and arguments calling the age of dog by adding value

--- Activities_Functions/script/script.js
+++ Activities_Functions/script/script.js
@@ -1,18 +1,35 @@
-unction outMsg(){
-	console.log("Hello World"); //print the message defines by function
+//function outMsg(){
+	//console.log("Hello World"); //print the message defines by function
+//}
+
+//var width = 5; //variable scope
+
+ //function calcArea(){
+ 	//var width = 20;
+ 	//var height = 30;
+ 	//var area = widht * height; //calculates the variable area by variable width * variable height
+ 	//console.log(area); //print the message defines by function
+ //}
+
+ //calcArea(); //invokes the function
+ //calcArea();
+ //calcArea();
+
+//console.log(width); //print variable widht
+
+//calArea(30, 20); //Arguments
+//function calArea(w, h){ // Parameters w=30, h=20
+	//var area = w * h; 
+	//console.log(area); //print out the area
+//}
+
+function dogYears(age){ //parameters store the value
+	var age =4;
+	var doYears = age * 7;
+	console.log("Sparky is " + dogYears + "years old.")
 }
 
-var width = 5; //variable scope
-
- function calcArea(){
- 	var width = 20;
- 	var height = 30;
- 	var area = widht * height; //calculates the variable area by variable width * variable height
- 	console.log(area); //print the message defines by function
- }
-
- calcArea(); //invokes the function
- calcArea();
- calcArea();
-
-console.log(width); //print variable widht
+var age1 = 4;
+dogYears(age1); //arguments
+dogYears(4); //call the function
+dogYears(6); //call the function
