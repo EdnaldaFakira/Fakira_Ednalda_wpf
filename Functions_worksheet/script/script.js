8255f9f843d6563cba9f922bfcaf7c07b3d0b669
Ednalda <ednaldafakira@gmail.com>
2013-05-29 15:22:59 -0400

    add comment to function calcCircumference explaining this function holds the parameter radius

--- Functions_worksheet/script/script.js
+++ Functions_worksheet/script/script.js
@@ -4,7 +4,7 @@
 
 var totalCircumference = calcCircunference(5);//Defines the variable and defines value as the variable calcCircumference and values as 5 and 2
 
-function calcCircunference(radius){
+function calcCircunference(radius){ //Defines the function that hold the parameter radius
 var circumference = radius * 3.14;//Defines the variable circumference and defines value as variable diameter / 2.
 return circumference //function spitting the info out
 }
