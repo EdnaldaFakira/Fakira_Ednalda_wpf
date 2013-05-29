6e026fc013250ef5938afb4eeb7f032121247c49
Ednalda <ednaldafakira@gmail.com>
2013-05-29 15:45:32 -0400

    change the function to anonymous functions assigning the var totalCircumference to function calcCircunference parameter "radius"

--- Functions_worksheet/script/script.js
+++ Functions_worksheet/script/script.js
@@ -4,12 +4,12 @@
 
 //Calculates the circumference of the circle
 
-var totalCircumference = calcCircumference(5);//Defines the variable and defines value as the variable calcCircumference and values as 5 and 2
+var totalCircumference = function calcCircumference(radius){ //Defines the function that hold the parameter radiuscalcCircumference(5);//Defines the variable and defines value as the variable calcCircumference and values as 5 and 2
 
-function calcCircumference(radius){ //Defines the function that hold the parameter radius
-var circumference = radius * 3.14;//Defines the variable circumference and defines value as radius * 3.14 (circumference / diameter = Pi 3.14).
-return circumference //function spitting the info out
+var circumference = radius * 3.14;//Defines the variable circumference and defines value as radius * 3.14 (circumference / diameter = Pi 3.14 "always").
+return circumference; //function spitting the info out
 }
+
 
 console.log("The circumference of the circle is" + totalCircumference); //print out the totalCircumference as a message
 
