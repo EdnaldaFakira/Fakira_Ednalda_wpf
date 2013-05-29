a3bc7da1d06bdedb9e3b37b33844e85aa83b2214
Ednalda <ednaldafakira@gmail.com>
2013-05-29 13:06:13 -0400

    add the var totalCircumference and defines the value as variable calccircumference and values 5 and 2

--- Functions_worksheet/script/script.js
+++ Functions_worksheet/script/script.js
@@ -2,7 +2,15 @@
 
 //Calculates the circumference of the circle
 
-var diameter = 5;
-var radiusCircle = calculates(diameter / 2);
+var totalCircumference = calcCircunference(5, 2);//Defines the variable and defines value as the variable calcCircumference and values as 5 and 2
+
+function calcCircunference(diameter, 2){
+var circumference = diameter / 2;//Defines the variable circumference and defines value as variable diameter / 2.
+return var = (circumference); //function spitting the info out
+}
+
+console.log("The circumference of the circle is" + totalCircumference);
+
+
 
 
