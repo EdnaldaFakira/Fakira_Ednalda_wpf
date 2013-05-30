3758badaf413e98b0144e661a07f64fa94aa4b6f
Ednalda <ednaldafakira@gmail.com>
2013-05-29 21:13:36 -0400

    declares variables and defines values as prompt command to enter house size, house model, if needs garage, and the type of floor whish. Also, declares variables and defines values as price for floor, carpet and tile floors.

--- Assignment_Functions/Personal/script/scrip.js
+++ Assignment_Functions/Personal/script/scrip.js
@@ -1,1 +1,15 @@
 //Ednalda Fakira Assignment Functions Personal 05/29/13
+
+//Manufacturing Houses
+
+var houseSize = prompt("Please enter the size of the house you whish to build");//declares variable houseSize and defines value as prompt command to enter house size
+var houseModel = prompt("Please enter the model of the house you whish to build");//declares variable houseModel and defines value as prompt command to enter the house model
+var garage = prompt("Please, enter yes if you want a garage, or no if you don't want a garage");//declares variable and defines value as prompt command to enter the yes or no
+var floor = prompt("Please enter the floor");//declares variable and defines value as prompt command to enter floor choice
+
+var woodFloor = 20;//declares variable woodFloor and defines value as $20
+var carpetFloor = 5;//declares variable carpetFloor and defines value as $5
+var tileFloor = 10;//declares variable tileFloor and defines value as $10
+
+
+
