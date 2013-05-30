e2f8f372abdb42d1192cf87cedf54aefe63b5079
Ednalda <ednaldafakira@gmail.com>
2013-05-29 23:04:35 -0400

    add the "if else" statement to decide want the garage and print out the totalHouse value of the house. Also the "else" statement to decide do not want garage and the totalHouse value of the house

--- Assignment_Functions/Personal/script/scrip.js
+++ Assignment_Functions/Personal/script/scrip.js
@@ -3,9 +3,8 @@
 //Manufacturing Houses
 
 var houseSize = prompt("Please enter the size of the house you whish to build");//declares variable houseSize and defines value as prompt command to enter house size
-var houseModel = prompt("Please enter the model of the house you whish to build");//declares variable houseModel and defines value as prompt command to enter the house model
 var garage = prompt("Please, enter yes if you want a garage, or no if you don't want a garage");//declares variable and defines value as prompt command to enter the yes or no
-var floor = prompt("Please enter the floor");//declares variable and defines value as prompt command to enter floor choice
+
 
 var pricePerSqF = function calcHouseSize (area){//declares variable pricePerSqF and defines value as function holding parameter "area"
 	var calcAreaPrice = area * 130;//declares variable calAreaPrice and defines value as area * $130
@@ -17,3 +16,16 @@ var size = pricePerSqF (houseSize);//declares variable size and defines value as
 
 console.log("The total area of your house will cost $" + calcAreaPrice);//print out the house cost based in the area chose
 
+var totalHouse = calcAreaPrice + 20,000;//declares variable toralHouse and defines value as calArePrice + $20,000
+
+if(garage = yes && houseSize != "")//decides if wants garage
+	console.log("We can build your house for $" + totalHouse);//print out the total value of the house
+}
+
+if else(garage = no && house != ""){//decides if don't want the garage 
+	console.log("We can build your house for $" + calAreaPrice);//print out the value of the house
+}
+
+else{
+	console.log("You need to enter the size of the house and decide if you want garage");//print out the message to reminder client to enter information required 
+}
